d582e31f5a7971512c37b666fa4fa8717bc528fe
Andrejs <extabezg@sanoma.com>
2017-10-04 16:47:23 +0300

    Configure frontend/backend

--- src/main/java/hello/RestCallsController.java
+++ src/main/java/hello/RestCallsController.java
@@ -3,11 +3,15 @@ package hello;
 import hello.Entity.ToDoEntity;
 import hello.Repository.ToDoEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class RestCallsController {
@@ -15,28 +19,35 @@ public class RestCallsController {
     @Autowired
     ToDoEntityRepository toDoEntityRepository;
 
-    @RequestMapping("/insert/{job}")
-    public Boolean insert(@PathVariable String job) {
+    @RequestMapping(method = POST, value = "/api/insert/{job}")
+    public ToDoEntity insert(@PathVariable String job) {
         ToDoEntity toDoEntity = new ToDoEntity();
         toDoEntity.setContent(job);
-        toDoEntityRepository.save(toDoEntity);
-        return true;
+        return toDoEntityRepository.save(toDoEntity);
     }
 
-    @RequestMapping("/delete/{id}")
+    @RequestMapping(method = POST,value = "/api/delete/{id}")
     public Boolean delete(@PathVariable int id) {
         toDoEntityRepository.delete(id);
         return true;
     }
 
-    @RequestMapping("/data")
-    public Iterable<ToDoEntity> requestList() {
-        return toDoEntityRepository.findAll();
+    @RequestMapping("/api/data")
+    public Iterable<ToDoEntity> requestList(@RequestParam(required = false) Boolean done) {
+        if (done == null) {
+            return toDoEntityRepository.findAll();
+        }
+        return toDoEntityRepository.findByDone(done);
     }
 
-    @RequestMapping("/toDo")
-    public List<ToDoEntity> requestNotDone() {
-        return toDoEntityRepository.findByDone(false);
+    @RequestMapping("/api/markDone/{id}/{done}")
+    public ToDoEntity markAsDone(@PathVariable int id, @PathVariable Boolean done) {
+        ToDoEntity toDoEntity = toDoEntityRepository.findById(id);
+        if (toDoEntity == null) {
+            return null;
+        }
+        toDoEntity.setDone(done);
+        return toDoEntityRepository.save(toDoEntity);
     }
 
 }
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        './src/main/resources/static/main'
+    ],
+    watchOptions: {
+        poll: true
+    },
+    output: {
+        path: path.join(__dirname, './build/resources/main/static/'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx"],
+        modules: [
+            path.join(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+    resolveLoader: {
+        modules: ['node_modules'],
+        extensions: [".js", ".json", ".jsx"],
+        mainFields: ["loader", "main"]
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            query: {
+                plugins: ['transform-decorators-legacy' ],
+                presets: ['es2015', 'stage-1', 'react']
+            }
+        },
+        {
+                test: /\.json?$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$/i,
+                loader: 'url-loader?limit=10000'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            }
+        ]
+    }
+};
